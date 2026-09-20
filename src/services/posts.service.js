cf968316ec6971e8ad0b00e078af2c150ac0c96c
neto <uoct50@gmail.com>
2026-09-19 22:32:45 -0300

    Criado o service e feta a leitura dos posts

---
+++ src/services/posts.service.js
@@ -0,0 +1,10 @@
+import fs from 'node:fs/promises';
+
+export async function getPosts(){
+    const data = await fs.readFile("././data/posts.json", "utf-8");
+    const posts = JSON.parse(data);
+
+    return posts; 
+};
+
+getPosts();
