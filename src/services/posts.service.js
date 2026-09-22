3e726354855c7a69a0dd67db8f2c2fe074498abb
neto <uoct50@gmail.com>
2026-09-21 23:28:33 -0300

    Fix: caminho do arquivo configurado de forma dinâmica

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,7 +1,9 @@
 import fs from 'node:fs/promises';
 
+const fileURL = new URL('../../data/posts.json', import.meta.url);
+
 export async function getPosts(){
-    const data = await fs.readFile("././data/posts.json", "utf-8");
+    const data = await fs.readFile(fileURL, "utf-8");
     const posts = JSON.parse(data);
 
     return posts; 
